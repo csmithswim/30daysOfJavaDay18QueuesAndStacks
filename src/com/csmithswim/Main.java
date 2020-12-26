951a606c702a645f7619a34933a68a0aaadd7a4e
Cullen S <cullensmi@gmail.com>
2020-12-25 22:12:18 -0500

    took away some comments

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -4,13 +4,6 @@ import java.util.*;
 
 
 public class Main {
-/*
-To solve this challenge, we must first take each character in , enqueue it in a queue, and also push that same character onto a stack. Once that's done, we must dequeue the first character from the queue and pop the top character off the stack, then compare the two characters to see if they are the same; as long as the characters match, we continue dequeueing, popping, and comparing each character until our containers are empty (a non-match means
-
-isn't a palindrome).
-
-Write the following declarations and implementations:
- */
     //sample input
     //racecar
 
